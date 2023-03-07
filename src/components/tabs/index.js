0448d1fdd86deefc944030ee7eab5383b5297403
Narmatha Sethumathavan <sethurengaraju@gmail.com>
2023-03-07 11:58:58 +0530

    tabs Added.

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,27 +1,28 @@
+import React from 'react';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { Box, Tab } from '@mui/material';
-import React from 'react'
-;
 import Screen from '../menu/screen/Screen';
-const Tabs = (context) => {
-	const { setState, state: { tabs }} = context;
+import Filter from '../Filter';
 
-	return <TabContext value={ tabs }>
-		<Box sx={ { borderBottom: 1, borderColor: 'divider' } }>
-			<TabList onChange={ (event, value) => setState((state) => ({
-				...state,
-				tabs: value,
-			})) }
-			>
-				<Tab label="Item One" value="1"/>
-				<Tab label="Item Two" value="2"/>
-				<Tab label="Item Three" value="3"/>
-			</TabList>
-		</Box>
-		<TabPanel value="1"><Screen { ...context }/></TabPanel>
-		<TabPanel value="2">Item Two</TabPanel>
-		<TabPanel value="3">Item Three</TabPanel>
-	</TabContext>;
+const Tabs = (context) => {
+	const { setState, state: { tabs }, config: { tabLabel }} = context;
+
+	return (
+		<TabContext value={ tabs }>
+			<Box sx={ { borderBottom: 1, borderColor: 'divider' } }>
+				<TabList onChange={ (event, value) => setState((state) => ({
+					...state,
+					tabs: value,
+				})) }
+				>
+					{tabLabel.map((data, key) =>
+						<Tab key={ key } label={ data } value={ data }/>)}
+				</TabList>
+			</Box>
+			<TabPanel value="product"><Screen { ...context }/></TabPanel>
+			<TabPanel value="filter"><Filter { ...context }/> </TabPanel>
+			<TabPanel value="cart">cart</TabPanel>
+		</TabContext>);
 };
 
 export default Tabs;
