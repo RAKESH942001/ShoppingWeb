62331e4729ae3b3c230991a8748d26cc3cec4cab
Rakesh <nlrakesh44@gmail.com>
2023-03-07 13:06:12 +0530

    Introduced Product Price .

--- src/components/menu/screen/ProductDetails.js
+++ src/components/menu/screen/ProductDetails.js
@@ -2,10 +2,13 @@ import { Box } from '@mui/material';
 import React from 'react';
 import ProductImages from './ProductImages';
 import ProductName from './ProductName';
+import ProductPrice from './ProductPrice';
 
 const ProductDetails = (context) =>
 	<Box className="productDetails">
 		<ProductImages { ...context }/>
-		<ProductName { ...context }/></Box>;
+		<ProductName { ...context }/>
+		<ProductPrice { ...context }/>
+	</Box>;
 
 export default ProductDetails;
---
+++ src/components/menu/screen/ProductPrice.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box } from '@mui/material';
+const ProductPrice = (context) => {
+	const { data: { price }} = context;
+
+	return <Box className="price"><label>${price}.00</label></Box>;
+};
+
+export default ProductPrice;
