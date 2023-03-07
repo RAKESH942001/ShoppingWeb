4bae7bc18086f6b8b9ef7e427c5c6fe438f1ad3f
Rakesh <nlrakesh44@gmail.com>
2023-03-07 20:53:48 +0530

    Dusted .

--- src/components/Options.js
+++
@@ -1,8 +0,0 @@
-import { MenuItem } from '@mui/material';
-import React from 'react';
-
-const Options = ({ data }) => data.map((product, key) =>
-	<MenuItem key={ key }value={ product }>
-		{product}</MenuItem>);
-
-export default Options;
