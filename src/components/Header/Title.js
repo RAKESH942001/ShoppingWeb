dca9aa9cf4f8a0b3b202d9a104c7bedcbb68ef17
Narmatha Sethumathavan <sethurengaraju@gmail.com>
2023-03-08 17:53:26 +0530

    Added title comp.

--- src/components/Header/Title.js
+++ src/components/Header/Title.js
@@ -2,12 +2,7 @@ import { React } from 'react';
 
 const Title = () =>
 	<div>
-		<img
-			src="/Image/powerup.jpeg"
-			alt="fs"
-			width="30px"
-			height="20px"
-		/>
+
 		<span>Hale & Hearty</span>
 	</div>;
 
