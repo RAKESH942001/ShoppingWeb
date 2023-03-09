4c1ebaad3c69444aa4a687fa90ec93ae7a545016
Narmatha Sethumathavan <sethurengaraju@gmail.com>
2023-03-09 11:41:30 +0530

    Modified css filter.

--- src/components/Header/Display.js
+++ src/components/Header/Display.js
@@ -1,17 +1,19 @@
 import { React } from 'react';
 import ProductDetails from '../products/ProductDetails';
 import cartManager from '../../services/cartManager';
+import { Box } from '@mui/material';
 
 const Display = (context) => {
 	const filteredProducts = cartManager.getFilter(context);
 
 	return (
-		filteredProducts.map((product, key) =>
-			<ProductDetails
-				key={ key }
-				className="container"
-				{ ...{ ...context, data: product } }
-			/>));
+		<Box className="filterContainer">
+			{filteredProducts.map((product, key) =>
+				<ProductDetails
+					key={ key }
+					{ ...{ ...context, data: product } }
+				/>)}
+		</Box>);
 };
 
 export default Display;
