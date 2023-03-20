fcec46f9ab67441077931bee5c18087a8238cfbe
Rakesh <nlrakesh44@gmail.com>
2023-03-20 09:06:34 +0530

    Introduced AppBar.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,4 @@
 import { React } from 'react';
-import AdImage from './AdImage';
 import BrandLogo from './BrandLogo';
 import Categories from './Categories';
 import Home from './Home';
@@ -8,18 +7,24 @@ import CartIcon from './CartIcon';
 import FaqButton from './FaqButton';
 import FavouriteIcon from './FavouriteIcon';
 import Login from './Login';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 
 const Header = (context) =>
-	<div className="menu">
-		<BrandLogo { ...context }/>
-		<Categories { ...context }/>
-		<About { ...context }/>
-		<FaqButton { ...context }/>
-		<Login { ...context }/>
-		<Home { ...context }/>
-		<FavouriteIcon { ...context }/>
-		<CartIcon { ...context }/>
-		<AdImage { ...context }/>
-	</div>;
+	<Box>
+		<AppBar className="header">
+			<Toolbar>
+				<BrandLogo { ...context }/>
+				<Categories { ...context }/>
+				<About { ...context }/>
+				<FaqButton { ...context }/>
+				<Login { ...context }/>
+				<Home { ...context }/>
+				<FavouriteIcon { ...context }/>
+				<CartIcon { ...context }/>
+			</Toolbar>
+		</AppBar>
+	</Box>;
 
 export default Header;
