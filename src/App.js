3cbf3912fbbe96cd746a51289d47936f75c3c225
Rakesh <nlrakesh44@gmail.com>
2023-03-20 09:05:36 +0530

    AdImage Refactored.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import { React } from 'react';
 import './App.scss';
+import AdImage from './components/body/AdImage';
 import Tabs from './components/body/tabs';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header';
@@ -14,6 +15,7 @@ const App = (context) => {
 	return (
 		<Box className="App">
 			<Header { ...context }/>
+			<AdImage { ...context }/>
 			<Tabs { ...context }/>
 			<Footer { ...context }/>
 		</Box>);
