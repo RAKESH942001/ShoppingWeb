94bd4b2d6a51c8f230edd76a0a01ce1e181a5e7f
Narmatha Sethumathavan <sethurengaraju@gmail.com>
2023-04-07 09:11:53 +0530

    Modified app comp.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { Box } from '@mui/material';
 import { React } from 'react';
 import './App.scss';
-import ResponsiveDrawer from './components/ResponsiveDrawer';
-import cartManager from './services/cartManager';
+import Screen from './components/Screen';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const App = (context) => {
-	const { once } = context;
-
-	once(() => cartManager.getProductAd(context));
-
-	return (
-		<Box className="App">
-			<ResponsiveDrawer { ...context }/>
-		</Box>);
-};
+const App = (context) =>
+	<Box className="App">
+		<Screen { ...context }/>
+	</Box>;
 
 export default App;
